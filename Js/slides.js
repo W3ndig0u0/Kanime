577554b317016469e4c26829cbf64736036d6bf4
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-06-01 15:01:25 +0200

    Background change

--- Js/slides.js
+++ Js/slides.js
@@ -6,12 +6,22 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("newsCard");
-  if (n > slides.length) {slideIndex = 1}    
+  var menu = document.querySelector(".menu");  
+
+  var colorOne = "rgba(5, 131, 242, 0.87)";  
+  var colorTwo = "rgba(5, 131, 242, 0.99)";  
+  var orientation = "to bottom";
+
+  var imgUrl = ["url(https://i.pinimg.com/originals/5f/12/b9/5f12b974076736afe4bfe170b36b5e89.jpg)", "url(https://wallpaperaccess.com/full/5918531.jpg)", "url(https://www.funimationfilms.com/wp-content/uploads/2021/03/VioletEvergardenMovie_BackgroundwLogo.jpg)", "url(https://i1.wp.com/www.monstersandcritics.com/wp-content/uploads/2020/08/Miss-Kobayashis-Dragon-Maid-Season-2-release-date-2021-Kyoto-Animation-Kobayashi-san-Chi-no-Maid-S.jpg?fit=1200%2C675&ssl=1)"];
+
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
   }
+
+  menu.style.backgroundImage = "linear-gradient("+ orientation + ", " + colorOne + ", " + colorTwo + ")," + (imgUrl[slideIndex-1]);
   slides[slideIndex-1].style.display = "block";  
 }
