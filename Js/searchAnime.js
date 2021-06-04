e6778a7126df10a46ae429f3017c0edba3f6d5c5
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-06-04 12:12:36 +0200

    Menu

--- Js/searchAnime.js
+++ Js/searchAnime.js
@@ -7,16 +7,34 @@ function searchAnime(event){
 
     const form = new FormData(this);
     const query = form.get("search");
+    const query2 = form.get("searchMobile");
 
-    fetch(`${base_url}/search/anime?q=${query}&page=1`)
-    .then(res=>res.json())
-    .then(updateDom)
-    .catch(err=>console.warn(err.message));
+    // !kollar om det e mobil eller vanlig e tom
+    if (query === undefined || query === null) {
+        fetch(`${base_url}/search/anime?q=${query2}&page=1`)
+        .then(res=>res.json())
+        .then(updateDom)
+        .catch(err=>console.warn(err.message));
+    }
+
+    else if (query2 === undefined || query2 === null){
+        fetch(`${base_url}/search/anime?q=${query}&page=1`)
+        .then(res=>res.json())
+        .then(updateDom)
+        .catch(err=>console.warn(err.message));
+    }
+
+    else{
+        fetch(`${base_url}/search/anime?q=${query}&page=1`)
+        .then(res=>res.json())
+        .then(updateDom)
+        .catch(err=>console.warn(err.message));
+    }
 }
 
 function updateDom(data){
 
-    const searchResults = document.getElementById('search-results');
+    const searchResults = document.querySelector('#search-results');
 
     const animeByCategories = data.results
         .reduce((acc, anime)=>{
@@ -64,6 +82,9 @@ function updateDom(data){
 function pageLoaded(){
     const form = document.getElementById('search_form');
     form.addEventListener("submit", searchAnime);
+
+    const formMobile = document.getElementById('searchFormMobil');
+    formMobile.addEventListener("submit", searchAnime);
 }
 
 window.addEventListener("load", pageLoaded);
