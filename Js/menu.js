3a64ebd4309ce4d2fbf2f63f363e59d43d5d019c
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-06-05 20:32:49 +0200

    a

--- Js/menu.js
+++ Js/menu.js
@@ -14,12 +14,12 @@ function closeNavbar() {
 
 document.querySelector(".search-btn").onclick = function() {
     document.getElementById("myModal").style.display = "block";
-    document.querySelector("body").style.overflow = "hidden";
+    document.querySelector("body").style.overflowY = "hidden";
 }
 
 window.onclick = function(event) {
     if (event.target == document.getElementById("myModal")) {
         document.getElementById("myModal").style.display = "none";
-        document.querySelector("body").style.overflow = "auto";
+        document.querySelector("body").style.overflowY = "auto";
     }
 }
