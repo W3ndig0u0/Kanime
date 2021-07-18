3fc92b741ef8038c75728acfeac100f3f0e727dc
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-07-18 16:09:48 +0200

    asdasd

--- Js/menu.js
+++ Js/menu.js
@@ -1,15 +1,12 @@
 function openNavbar() {
-    document.getElementById("closeButton").style.display = "block"; 
-    document.getElementById("hamburgerNavbarLinks").style.display = "block";
-    document.querySelector("body").classList.toggle("active");  
     document.getElementById("hamburgerNavbarLinks").style.width = "100%";
-}
-
-function closeNavbar() {
-    document.getElementById("hamburgerNavbarLinks").style.width = "0%";
-    document.getElementById("closeButton").style.display = "none";
     document.querySelector("body").classList.toggle("active");
-}
+  }
+  
+  function closeNavbar() {
+    document.getElementById("hamburgerNavbarLinks").style.width = "0%";
+    document.querySelector("body").classList.toggle("active");
+  }
 
 
 document.querySelector(".search-btn").onclick = function() {
