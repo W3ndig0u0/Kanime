dbd822046aca9a0a7ef1fc6b19ab18cf0b0a5214
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-08-16 17:55:49 +0200

    YEP

--- Js/menu.js
+++ Js/menu.js
@@ -19,3 +19,44 @@ window.onclick = function(event) {
         document.querySelector("body").style.overflowY = "auto";
     }
 }
+
+// !login
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+const signInBtnGoogle = document.getElementById('signInBtnGoogle');
+
+const userDetails = document.getElementById('userDetails');
+const userImg = document.getElementById('userImg');
+
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtnGoogle.onclick = () => auth.signInWithPopup(googleProvider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.className = "not-hidden";
+        whenSignedOut.className = "hidden";
+        userImg.innerHTML = `<img class="user-img" src="${user.photoURL}">`;
+        userDetails.innerHTML = `<h3 class="name">Welcome ${user.displayName}!</h3>`;
+    } 
+    else {
+        // not signed in
+        whenSignedIn.className = "hidden";
+        whenSignedOut.className = "not-hidden";
+        userDetails.innerHTML = '';
+        userImg.innerHTML = '';
+    }
+});
+
+
+document.querySelector('.drop').onclick = () => {
+  document.querySelector(".dropdown-item").classList.toggle("active");
+}
